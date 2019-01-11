49cf601f6c204ecd8948ecda41c000cca223d1de
jhweng <juei.weng@inf.ufrgs.br>
2019-01-11 19:26:48 -0200

    Fix keywords search on Twitter loop

--- newsExtractor/main/extractor.py
+++ newsExtractor/main/extractor.py
@@ -27,9 +27,9 @@ def json2obj(data):
 # =======================
 
 # Set the limit for number of articles to download
-LIMIT = 3
+num_of_articles = 3
 # Minimum frequency of a word to be consider as keyword
-minFrequence = 6
+minFrequence = 8
 # Number of tweets search
 num_of_tweets_search = 4
 # Toggle for cmd line runtime comments
@@ -73,148 +73,148 @@ with open(sources_urls) as data_file:
 count = 1
 
 # Iterate through each news company
-for company, value in companies.items():
-    company_dir = news_dir + str(company) + '/'
-    if not os.path.exists(company_dir):
-        if _comments:
-            print('Creating folder ' + company_dir)
-        os.makedirs(company_dir)
-
-    # If a RSS link is provided in the JSON file, this will be the first choice.
-    # Reason for this is that, RSS feeds often give more consistent and correct data.
-    if 'rss' in value:
-        d = fp.parse(value['rss'])
-        if _comments:
-            print("Downloading articles from ", company)
-        newsPaper = {
-            "rss": value['rss'],
-            "link": value['link'],
-            "articles": []
-        }
-        for entry in d.entries:
-            # Check if publish date is provided, if no the article is skipped.
-            # This is done to keep consistency in the data and to keep the script from crashing.
-            if hasattr(entry, 'published'):
-                if count > LIMIT:
-                    break
-                article = {}
-                article['link'] = entry.link
-                date = entry.published_parsed
-                article['published'] = datetime.fromtimestamp(mktime(date)).isoformat()
-                try:
-                    content = Article(entry.link)
-                    content.download()
-                    content.parse()
-                except Exception as e:
-                    # If the download for some reason fails (ex. 404) the script will continue downloading
-                    # the next article.
-                    print(e)
-                    print("continuing...")
-                    continue
-                article['title'] = content.title
-                article['text'] = content.text
-                newsPaper['articles'].append(article)
-                if _comments:
-                    print(count, "articles downloaded from", company, ", url: ", entry.link)
-                count = count + 1
-    else:
-        # This is the fallback method if a RSS-feed link is not provided.
-        # It uses the python newspaper library to extract articles
-        if _comments:
-            print("Building site for ", company)
-        paper = newspaper.build(value['link'], memoize_articles=False)
-        newsPaper = {
-            "link": value['link'],
-            "articles": []
-        }
-        noneTypeCount = 0
-        for content in paper.articles:
-            if count > LIMIT:
-                break
-            try:
-                content.download()
-                content.parse()
-            except Exception as e:
-                print(e)
-                print("continuing...")
-                continue
-            # Again, for consistency, if there is no found publish date the article will be skipped.
-            # After 10 downloaded articles from the same newspaper without publish date, the company will be skipped.
-            if content.publish_date is None:
-                if _comments:
-                    print(count, " Article has date of type None...")
-                noneTypeCount = noneTypeCount + 1
-                if noneTypeCount > 10:
-                    if _comments:
-                        print("Too many noneType dates, aborting...")
-                    noneTypeCount = 0
-                    break
-                count = count + 1
-                continue
-            article = {}
-            article['title'] = content.title
-            article['text'] = content.text
-            article['link'] = content.url
-            article['published'] = content.publish_date.isoformat()
-            newsPaper['articles'].append(article)
-            if _comments:
-                print(count, "articles downloaded from", company, " using newspaper, url: ", content.url)
-            count = count + 1
-            noneTypeCount = 0
-    count = 1
-    data['newspapers'][company] = newsPaper
-
-    # Finally it saves the articles as a JSON-file.
-    try:
-        with open(company_dir + company + '.json', 'w') as outfile:
-            json.dump(data, outfile)
-    except Exception as e:
-        print(e)
-
-    data['newspapers'] = {}
-
-if _comments:
-    print('News extraction finished.')
-
-# ==============================================================
-# ===========  Extracting keywords from news  ==================
-# ==============================================================
-
-for company, value in companies.items():
-    company_dir = news_dir + str(company) + '/'
-    if _comments:
-        print('Extracting keywords from ' + company_dir + company + '.json')
-    # extracting only text part of news json structure into an array/tuple
-    with open(company_dir + company + '.json', 'r') as myfile:
-        data = json.loads(myfile.read())
-        news_struct = objectpath.Tree(data['newspapers'])
-        result_tuple = tuple(news_struct.execute('$..text'))
-
-    # remove common words and tokenize
-    stopWordsList = set(stopwords.words('english'))
-    texts = [[word for word in document.lower().split() if word not in stopWordsList]
-              for document in result_tuple]
-
-    frequency = defaultdict(int)
-    for text in texts:
-        for token in text:
-            frequency[token] += 1
-
-    # remove words that appear only minFreq times
-    texts = [[token for token in text if frequency[token] > minFrequence]
-              for text in texts]
-
-    text_index = 0
-    sets_of_keywords = []
-    for text in texts:
-        text_index += 1
-        with open(company_dir + "keywords" + str(text_index) + ".txt", "w") as text_file:
-            for single_word in set(text):
-                print(single_word, file=text_file)
-        sets_of_keywords.append(set(text))
-
-
-    print("Keywords extracted to " + company_dir + "keywords.txt")
+# for company, value in companies.items():
+#     company_dir = news_dir + str(company) + '/'
+#     if not os.path.exists(company_dir):
+#         if _comments:
+#             print('Creating folder ' + company_dir)
+#         os.makedirs(company_dir)
+#
+#     # If a RSS link is provided in the JSON file, this will be the first choice.
+#     # Reason for this is that, RSS feeds often give more consistent and correct data.
+#     if 'rss' in value:
+#         d = fp.parse(value['rss'])
+#         if _comments:
+#             print("Downloading articles from ", company)
+#         newsPaper = {
+#             "rss": value['rss'],
+#             "link": value['link'],
+#             "articles": []
+#         }
+#         for entry in d.entries:
+#             # Check if publish date is provided, if no the article is skipped.
+#             # This is done to keep consistency in the data and to keep the script from crashing.
+#             if hasattr(entry, 'published'):
+#                 if count > num_of_articles:
+#                     break
+#                 article = {}
+#                 article['link'] = entry.link
+#                 date = entry.published_parsed
+#                 article['published'] = datetime.fromtimestamp(mktime(date)).isoformat()
+#                 try:
+#                     content = Article(entry.link)
+#                     content.download()
+#                     content.parse()
+#                 except Exception as e:
+#                     # If the download for some reason fails (ex. 404) the script will continue downloading
+#                     # the next article.
+#                     print(e)
+#                     print("continuing...")
+#                     continue
+#                 article['title'] = content.title
+#                 article['text'] = content.text
+#                 newsPaper['articles'].append(article)
+#                 if _comments:
+#                     print(count, "articles downloaded from", company, ", url: ", entry.link)
+#                 count = count + 1
+#     else:
+#         # This is the fallback method if a RSS-feed link is not provided.
+#         # It uses the python newspaper library to extract articles
+#         if _comments:
+#             print("Building site for ", company)
+#         paper = newspaper.build(value['link'], memoize_articles=False)
+#         newsPaper = {
+#             "link": value['link'],
+#             "articles": []
+#         }
+#         noneTypeCount = 0
+#         for content in paper.articles:
+#             if count > num_of_articles:
+#                 break
+#             try:
+#                 content.download()
+#                 content.parse()
+#             except Exception as e:
+#                 print(e)
+#                 print("continuing...")
+#                 continue
+#             # Again, for consistency, if there is no found publish date the article will be skipped.
+#             # After 10 downloaded articles from the same newspaper without publish date, the company will be skipped.
+#             if content.publish_date is None:
+#                 if _comments:
+#                     print(count, " Article has date of type None...")
+#                 noneTypeCount = noneTypeCount + 1
+#                 if noneTypeCount > 10:
+#                     if _comments:
+#                         print("Too many noneType dates, aborting...")
+#                     noneTypeCount = 0
+#                     break
+#                 count = count + 1
+#                 continue
+#             article = {}
+#             article['title'] = content.title
+#             article['text'] = content.text
+#             article['link'] = content.url
+#             article['published'] = content.publish_date.isoformat()
+#             newsPaper['articles'].append(article)
+#             if _comments:
+#                 print(count, "articles downloaded from", company, " using newspaper, url: ", content.url)
+#             count = count + 1
+#             noneTypeCount = 0
+#     count = 1
+#     data['newspapers'][company] = newsPaper
+#
+#     # Finally it saves the articles as a JSON-file.
+#     try:
+#         with open(company_dir + company + '.json', 'w') as outfile:
+#             json.dump(data, outfile)
+#     except Exception as e:
+#         print(e)
+#
+#     data['newspapers'] = {}
+#
+# if _comments:
+#     print('News extraction finished.')
+#
+# # ==============================================================
+# # ===========  Extracting keywords from news  ==================
+# # ==============================================================
+#
+# for company, value in companies.items():
+#     company_dir = news_dir + str(company) + '/'
+#     if _comments:
+#         print('Extracting keywords from ' + company_dir + company + '.json')
+#     # extracting only text part of news json structure into an array/tuple
+#     with open(company_dir + company + '.json', 'r') as myfile:
+#         data = json.loads(myfile.read())
+#         news_struct = objectpath.Tree(data['newspapers'])
+#         result_tuple = tuple(news_struct.execute('$..text'))
+#
+#     # remove common words and tokenize
+#     stopWordsList = set(stopwords.words('english'))
+#     texts = [[word for word in document.lower().split() if word not in stopWordsList]
+#               for document in result_tuple]
+#
+#     frequency = defaultdict(int)
+#     for text in texts:
+#         for token in text:
+#             frequency[token] += 1
+#
+#     # remove words that appear only minFreq times
+#     texts = [[token for token in text if frequency[token] > minFrequence]
+#               for text in texts]
+#
+#     text_index = 0
+#     sets_of_keywords = []
+#     for text in texts:
+#         text_index += 1
+#         with open(company_dir + "keywords" + str(text_index) + ".txt", "w") as text_file:
+#             for single_word in set(text):
+#                 print(single_word, file=text_file)
+#         sets_of_keywords.append(set(text))
+#
+#
+#     print("Keywords extracted to " + company_dir + "keywords.txt")
 
 
 # ==============================================================
@@ -229,19 +229,16 @@ api = tweepy.API(auth)
 user = api.me()
 print('Tweepy API user: ' + user.name)
 
-keywords_set_index = 0
 for company, value in companies.items():
     company_dir = news_dir + str(company) + '/'
-    keywords_set_index = 1
+    str_from_date = '2018-12-03'
+    str_search_term = ''
 
-    with open(company_dir + 'keywords' + str(keywords_set_index) + '.txt', 'r') as keyword_file:
-        keywords_list = keyword_file.read()
+    for article_index in range(num_of_articles):
+        print('Openning ' + company_dir + 'keywords' + str(article_index+1) + '.txt')
+        with open(company_dir + 'keywords' + str(article_index+1) + '.txt', 'r') as keyword_file:
+            str_search_term = keyword_file.read().replace('\n', ' ')
 
-    for words_set in sets_of_keywords:
-        for single_word in words_set:
-            str_search_term = str_search_term + single_word + ' '
-
-        str_from_date = '2018-12-03'
 
         if _write_to_file:
             # Open/Create a file to append data
@@ -251,7 +248,7 @@ for company, value in companies.items():
         else:
             print('Write to csv file = ' + str(_write_to_file))
 
-        print('Searching on Twitter using \'' + str_search_term + '\' ...')
+        print('Searching for news of ' + str(company) + ' on Twitter using \'' + str_search_term + '\' ...')
         for tweet in tweepy.Cursor(api.search, tweet_mode='extended',
                                    q=str_search_term, since=str_from_date).items(num_of_tweets_search):
             print(tweet.created_at)
