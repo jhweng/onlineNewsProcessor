6222093940edbe4bb545b712fff49284ef4e43ee
Juei <juei.weng@sap.com>
2019-06-10 18:37:38 -0300

    improve tweepy search with api.search

--- newsExtractor/main/extractor.py
+++ newsExtractor/main/extractor.py
@@ -338,3 +338,73 @@ for company, value in companies.items():
 
 
 # verificar se o conteudo do tweet contem keywords da query
+
+# https://bhaskarvk.github.io/2015/01/how-to-use-twitters-search-rest-api-most-effectively./
+
+# import sys
+# import jsonpickle
+# import os
+# import json
+# import tweepy
+#
+# # Replace the API_KEY and API_SECRET with your application's key and secret.
+# auth = tweepy.AppAuthHandler('XXXX', 'OOOOOOO')
+#
+# api = tweepy.API(auth, wait_on_rate_limit=True,
+# 				   wait_on_rate_limit_notify=True)
+#
+# if (not api):
+#     print ("Can't Authenticate")
+#     sys.exit(-1)
+#
+# searchQuery = 'theoffice'  # this is what we're searching for
+# maxTweets = 1000 # Some arbitrary large number
+# tweetsPerQry = 100  # this is the max the API permits
+# fName = 'tweets.txt' # We'll store the tweets in a text file.
+#
+#
+# # If results from a specific ID onwards are reqd, set since_id to that ID.
+# # else default to no lower limit, go as far back as API allows
+# sinceId = None
+#
+# # If results only below a specific ID are, set max_id to that ID.
+# # else default to no upper limit, start from the most recent tweet matching the search query.
+# max_id = -1
+#
+# tweetCount = 0
+# print("Downloading max {0} tweets".format(maxTweets))
+# with open(fName, 'w') as f:
+#     while tweetCount < maxTweets:
+#         try:
+#             if (max_id <= 0):
+#                 if (not sinceId):
+#                     new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended')
+#                 else:
+#                     new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+#                                             since_id=sinceId)
+#             else:
+#                 if (not sinceId):
+#                     new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+#                                             max_id=str(max_id - 1))
+#                 else:
+#                     new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+#                                             max_id=str(max_id - 1),
+#                                             since_id=sinceId)
+#             if not new_tweets:
+#                 print("No more tweets found")
+#                 break
+#             for tweet in new_tweets:
+#                 json1_data = json.loads(jsonpickle.encode(tweet._json, unpicklable=False))
+#                 if (not 'RT @' in json1_data['full_text']):
+#                     print(json1_data['created_at'])
+#                     print(json1_data['full_text'])
+#                 break
+#             tweetCount += len(new_tweets)
+#             print("Downloaded {0} tweets".format(tweetCount))
+#             max_id = new_tweets[-1].id
+#         except tweepy.TweepError as e:
+#             # Just exit if any error
+#             print("some error : " + str(e))
+#             break
+#
+# print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
--- newsExtractor/main/forTesting.py
+++ newsExtractor/main/forTesting.py
@@ -1,13 +1,67 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
+import sys
+import jsonpickle
+import os
+import json
+import tweepy
 
-data = "Mr Trump had planned to spend Christmas at his private golf club in Florida, but stayed behind in Washington because of the current partial government shutdown."
-stopWords = set(stopwords.words('english'))
-words = word_tokenize(data)
-wordsFiltered = []
+# Replace the API_KEY and API_SECRET with your application's key and secret.
+auth = tweepy.AppAuthHandler('6E9bYOOKfL1rgTc8Qrof7tfZB', '6MMHlCzADmHbMWuT1TzOsGfKGJg17gNOBadulHpEkr3G0pvBBS')
 
-for w in words:
-    if w not in stopWords:
-        wordsFiltered.append(w)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+				   wait_on_rate_limit_notify=True)
 
-print(wordsFiltered)
+if (not api):
+    print ("Can't Authenticate")
+    sys.exit(-1)
+
+searchQuery = 'theoffice'  # this is what we're searching for
+maxTweets = 1000 # Some arbitrary large number
+tweetsPerQry = 100  # this is the max the API permits
+fName = 'tweets.txt' # We'll store the tweets in a text file.
+
+
+# If results from a specific ID onwards are reqd, set since_id to that ID.
+# else default to no lower limit, go as far back as API allows
+sinceId = None
+
+# If results only below a specific ID are, set max_id to that ID.
+# else default to no upper limit, start from the most recent tweet matching the search query.
+max_id = -1
+
+tweetCount = 0
+print("Downloading max {0} tweets".format(maxTweets))
+with open(fName, 'w') as f:
+    while tweetCount < maxTweets:
+        try:
+            if (max_id <= 0):
+                if (not sinceId):
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended')
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+                                            since_id=sinceId)
+            else:
+                if (not sinceId):
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+                                            max_id=str(max_id - 1))
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry, tweet_mode='extended',
+                                            max_id=str(max_id - 1),
+                                            since_id=sinceId)
+            if not new_tweets:
+                print("No more tweets found")
+                break
+            for tweet in new_tweets:
+                json1_data = json.loads(jsonpickle.encode(tweet._json, unpicklable=False))
+                if (not 'RT @' in json1_data['full_text']):
+                    print(json1_data['created_at'])
+                    print(json1_data['full_text'])
+                break
+            tweetCount += len(new_tweets)
+            print("Downloaded {0} tweets".format(tweetCount))
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            # Just exit if any error
+            print("some error : " + str(e))
+            break
+
+print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
